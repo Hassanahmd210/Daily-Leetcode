95a628d4f3be9f414127e95303e1a939bea92f67
Hassan ahmad Sayyad <154266974+Hassanahmd210@users.noreply.github.com>
2025-05-22 00:37:54 +0530

    Time: 0 ms (100%), Space: 9.1 MB (81.78%) - LeetHub

---
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int myAtoi(std::string s) {
+        int i = 0, sign = 1, result = 0;
+
+        while (i < s.length() && s[i] == ' ') {
+            i++;
+        }
+
+        if (i < s.length() && (s[i] == '-' || s[i] == '+')) {
+            sign = (s[i] == '-') ? -1 : 1;
+            i++;
+        }
+
+        while (i < s.length() && s[i] >= '0' && s[i] <= '9') {
+            int digit = s[i] - '0';
+
+            if (result > (INT_MAX - digit) / 10) {
+                return (sign == 1) ? INT_MAX : INT_MIN;
+            }
+
+            result = result * 10 + digit;
+            i++;
+        }
+
+        return result * sign;
+    }
+};
