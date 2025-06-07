180d9e721a01e7adc479b1a424f9b73d0ac7da70
Hassan ahmad Sayyad <154266974+Hassanahmd210@users.noreply.github.com>
2025-06-07 21:13:55 +0530

    Time: 0 ms (100%), Space: 11.8 MB (87.41%) - LeetHub

---
+++ 1310-watering-plants/1310-watering-plants.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int wateringPlants(vector<int>& plants, int capacity) {
+        int steps = 0;
+        int water = capacity;
+
+        for (int i = 0; i < plants.size(); i++) {
+            if (water < plants[i]) {
+                steps += i + i;
+                water = capacity;
+            }
+            water -= plants[i];
+            steps++;
+        }
+
+        return steps;
+    }
+};
