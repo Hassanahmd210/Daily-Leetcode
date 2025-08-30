2ace996562a1807f75c9ef6714f1ce7e5b4ea15e
Hassan ahmad Sayyad <154266974+Hassanahmd210@users.noreply.github.com>
2025-08-30 20:01:48 +0530

    Time: 4 ms (52.67%), Space: 25.9 MB (7.64%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.cpp
+++ 0036-valid-sudoku/0036-valid-sudoku.cpp
@@ -1,20 +1,25 @@
 class Solution {
 public:
     bool isValidSudoku(vector<vector<char>>& board) {
-        bool row[9][9] = {false};
-        bool col[9][9] = {false};
-        bool box[9][9] = {false};
+        
+        unordered_set<string> seen;
 
         for (int i = 0; i < 9; ++i) {
             for (int j = 0; j < 9; ++j) {
-                if (board[i][j] != '.') {
-                    int num = board[i][j] - '1';
-                    int bi = (i / 3) * 3 + (j / 3);
-                    
-                    if (row[i][num] || col[j][num] || box[bi][num])
-                        return false;
+                char num = board[i][j];
+                if (num != '.') {
 
-                    row[i][num] = col[j][num] = box[bi][num] = true;
+                    string rowKey = "row" + to_string(i) + "-" + num;
+                    string colKey = "col" + to_string(j) + "-" + num;
+                    string boxKey = "box" + to_string(i / 3) + to_string(j / 3) + "-" + num;
+
+                    if (seen.count(rowKey) || seen.count(colKey) || seen.count(boxKey)) {
+                        return false;
+                    }
+
+                    seen.insert(rowKey);
+                    seen.insert(colKey);
+                    seen.insert(boxKey);
                 }
             }
         }
